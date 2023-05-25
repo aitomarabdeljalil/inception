5175b4a7be43eac5b104de83f86f28dc825ce18a
Abdeljalil Ait omar <aait-oma@e2r3p11.1337.ma>
2023-05-25 18:46:41 +0100

    first push

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
+    service mysql start
+	echo "$DB_NAME database does not exist"
+    mysql -u root -e "CREATE DATABASE $DB_NAME;";
+    mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'" ;
+    mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'; FLUSH PRIVILEGES;" ;
+    mysql -u root -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" ;
+	kill `cat /var/run/mysqld/mysqld.pid`
+fi
+
+mysqld_safe
+# tail -f /dev/null
