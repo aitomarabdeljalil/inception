c6156280402904414b064752e11fe15c347595c7
Abdeljalil Ait omar <aait-oma@e2r3p11.1337.ma>
2023-05-31 13:37:29 +0100

    redis conf

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -7,6 +7,8 @@ if [ ! -f "/var/www/html/index.php" ]; then
 	runuser -u www-data -- wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost='mariadb' --skip-check --path=/var/www/html/ ;
 	runuser -u www-data -- wp user create $WP_ADMIN $WP_EMAIL --role=author --user_pass=$WP_PASS --path='/var/www/html/';
 	runuser -u www-data -- wp core install --path=/var/www/html/ --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADM_PASS  --admin_email=$WP_EMAIL;
+	runuser -u www-data -- wp config set WP_REDIS_PORT 6379 --path="/var/www/html";
+	runuser -u www-data -- wp config set WP_REDIS_HOST redis --path="/var/www/html";
 	runuser -u www-data -- wp plugin install redis-cache --activate;
 	sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf;
 fi
