bc61de23eb14fd0882766ca778c67ecee9fe59fd
Abdeljalil Ait omar <aait-oma@e2r3p11.1337.ma>
2023-06-01 13:25:37 +0100

    mariadb script update

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -6,7 +6,7 @@ if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
     mysql -u root -e "CREATE DATABASE $DB_NAME;";
     mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'" ;
     mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'; FLUSH PRIVILEGES;" ;
-    mysql -u root -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" ;
+    mysql -u root -e "alter user 'root'@'localhost' identified by '$ROOT_PSWD'; FLUSH PRIVILEGES;" ;
 	kill `cat /var/run/mysqld/mysqld.pid`
 fi
 
